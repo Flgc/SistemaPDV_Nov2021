d01cc7d7569cf7f9ecffc66b319d5131f849a47f
Fabio Luis <ml@fabiotecnico1.biz>
2021-11-18 19:11:42 -0300

    Implementando Internal Frame e Classe para carregar frames

---
+++ SistemaPDV21/src/principal/Inicializador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JDesktopPane;
+
+/**
+ *
+ * @author fabio
+ */
+public class Inicializador extends JDesktopPane{
+    Image img = new ImageIcon(getClass().getResource("/imagens/principal/imagemLogin.jpg")).getImage(); 
+    @Override
+    public void paintChildren(Graphics g) {
+        g.drawImage(img, 0, 0, this)
+    }   
+    
+}
