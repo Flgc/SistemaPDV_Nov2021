68f9bee9bf28a2a3015a4e7b01a486a1aa5232a2
Fabio Luis <ml@fabiotecnico1.biz>
2021-11-24 20:35:38 -0300

    Imlementando Classe de Conexão

---
+++ SistemaPDV21/src/dataBase/Conectar.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataBase;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fabio
+ */
+public class Conectar {
+    
+    Connection conect = null;
+    
+    public Connection conexao() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conect = DriverManager.getConnection("jdbc:mysql://localhost/NOMEDOBD\", \"USUARIO\", \"SENHA");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog("Error na conexão" + e);
+        }
+        return conect;
+}        
+}
