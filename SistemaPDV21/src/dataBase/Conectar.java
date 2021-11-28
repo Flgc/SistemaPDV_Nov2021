3ea206a531b75b9d60bdf6c92933d43c891c47fe
Fabio Luis <ml@fabiotecnico1.biz>
2021-11-28 09:10:50 -0300

    Conectando ao banco de dados - Troca de biblioteca de conexao e adição de usuário e senha

--- SistemaPDV21/src/dataBase/Conectar.java
+++ SistemaPDV21/src/dataBase/Conectar.java
@@ -12,6 +12,7 @@ import javax.swing.JOptionPane;
 /**
  *
  * @author fabio
+ * 
  */
 public class Conectar {
     
@@ -20,9 +21,9 @@ public class Conectar {
     public Connection conexao() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conect = DriverManager.getConnection("jdbc:mysql://localhost/sistemapdv21\", \"root\", \"");
+            conect = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistemapdv21", "fabio", "mysql2021");
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null,"Error ao contar no banco de dados" + e);
+            JOptionPane.showMessageDialog(null,"Error ao conectar no banco de dados -> " + e);
         }
         return conect;
    }        
