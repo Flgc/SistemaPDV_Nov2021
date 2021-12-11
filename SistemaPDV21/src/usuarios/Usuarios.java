a3a4e7ce32291a6cbe67a6ad21b1460f168e9ecc
Fabio Luis <ml@fabiotecnico1.biz>
2021-12-11 10:34:15 -0300

    Formulário Usuário - Implementando métodos para comunicação com o banco de dados - class Usuarios

---
+++ SistemaPDV21/src/usuarios/Usuarios.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usuarios;
+
+/**
+ *
+ * @author fabio
+ */
+public class Usuarios {
+    
+    //Constants
+    public static String LIST_US = "SELECT * FROM usuarios ORDER BY nome_usu";
+    
+    public static String INSERT_US = "INSERT INTO usuarios(codigo_us, nome_us,"
+            + "sexo_use, tipo_us, senha_us)" 
+            + "VALUES(?,?,?,?,?)";
+    
+    public static String UPDATE_US = "UPDATE usuarios SET "
+            + "nome_us = ?, "
+            + "sexo_us = ?, "
+            + "tipo_us = ?, "
+            + "senha_us = ? WHERE codigo_us=?";
+    
+    public static String DELETE_US = "DELETE FROM usuarios WHERE codigo_us = ?";
+    
+    public static String DELETE_ALL_US = "DELETE FROM usuarios";
+    
+    private String primaryKey;
+    private String nome;
+    private String sexo;
+    private String tipoUser;
+    private String senha;
+    
+   public Usuarios(){
+       
+   }
+   
+   public String getPrimaryKey(){
+       return primaryKey;
+   }
+   
+   public void setPrimaryKey(String primaryKey) {
+       this.primaryKey = primaryKey;
+   }
+   
+   public String getNome(){
+       return nome;
+   }
+   
+   public void setNome(String nome) {
+       this.nome = nome;
+   }
+
+   public String getSexo(){
+       return sexo;
+   }
+   
+   public void setSexo(String sexo) {
+       this.sexo = sexo;
+   }
+
+   public String getTipoUser(){
+       return tipoUser;
+   }
+   
+   public void setTipoUser(String tipoUser) {
+       this.tipoUser = tipoUser;
+   }
+
+   public String getSenha(){
+       return senha;
+   }
+   
+   public void setSenha(String senha) {
+       this.sexo = senha;
+   }   
+    
+}
